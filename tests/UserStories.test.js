088fb2ae72a69f19a0b71c14a07667790d2cd872
Ковтюх Александр <a.kovtukh@owox.com>
2014-09-07 19:02:14 +0300

    Починил заглушку для тестирования JSONP

--- tests/UserStories.test.js
+++ tests/UserStories.test.js
@@ -16,36 +16,72 @@ describe("UserStories tests", function () {
 
 	describe("method get", function () {
 
+		var json_response;
 		beforeEach(function () {
-
-			this.jsonp_send = sinon.stub(Request.JSONP.prototype, "send");
-
+			/*
+			эта переменная будет в тесте содержать json ответ сервера.
+			заполняем ее перед вызовом, который приведет к запросу на сервер
+			и получаем результат после обработки его тестируемым классом
+			*/
+			json_response = null;
+			this.jsonp_stub = sinon.stub(
+				Request.JSONP.prototype,
+				"getScript",
+				function (url) {
+					var request_func_name = new URI(url).get('data').callback;
+					var response = json_response.shift()||{};
+					var injected_element = new Element('script', {
+						type: 'text/javascript',
+						text: request_func_name + '('+ JSON.encode(response) + ')'
+					});
+					return injected_element;
+				}
+			);
 			this.jsonp_initialize = sinon.spy(Request.JSONP.prototype, "initialize");
-
 		});
 
 		afterEach(function () {
-			this.jsonp_send.restore();
-
+			this.jsonp_stub.restore();
+			this.jsonp_initialize.restore();
 		});
 
 		it("по вызову get, отправляется запрос на нужный адрес", function () {
-
-			var callback_stub = sinon.stub();
-
-			this.user_stories.get(callback_stub, "Id eq 13", "[Id]");
-
+			json_response = [{Items:[]}];
+			this.user_stories.get(Function.from(), "Id eq 13", "[Id]");
 			var url = new URI(this.jsonp_initialize.firstCall.args[0].url);
-
 			expect(url.get('host')).to.be('owox.tpondemand.com');
-
-			expect(this.jsonp_send.called).to.be.ok();
-
 		});
 
+		it(
+			"по завершению получения данных, вызывается переданный коллбек",
+			function () {
+				json_response = [{Items:[]}];
+				var callback_spy = sinon.spy();
+				this.user_stories.get(callback_spy, "Id eq 13", "[Id]");
+				expect(callback_spy.calledWith({stories:[]})).to.be.ok();
+			}
+		);
+
+		it(
+			'Если ответ сервера не поместился в одну пачку - запросы должны закольцовываться',
+			function ()  {
+				json_response = [
+					{
+						"Next": "https://some_url.com",
+						"Items": [{"Id": 24776}, {"Id": 24752}]
+					},
+					{
+						"Items": [{"Id": 24777}]
+					}
+				];
+				var callback_spy = sinon.spy();
+				this.user_stories.get(callback_spy, "Id eq 13", "[Id]");
+				//с сервера получены все три сущности
+				expect(callback_spy.firstCall.args[0].stories.length).to.be(3);
+				//сущности были получены в два захода
+				expect(this.jsonp_stub.callCount).to.be(2);
+			}
+		)
 	});
-
-
 });
-
 });
